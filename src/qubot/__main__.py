b2dcd93cf1123bd59d1477c3df4583c133eb1f1a
Marco <gobbo.marco98@gmail.com>
2025-06-20 17:58:00 +0200

    Add LN reminder

--- src/qubot/__main__.py
+++ src/qubot/__main__.py
@@ -1,5 +1,7 @@
 import discord
 from discord.ext import commands
+
+from ln_refill import ln_refill_setup
 from journal_club import journal_club_setup
 from mysecrets import DISCORD_TOKEN
 
@@ -9,6 +11,7 @@ bot = commands.Bot(command_prefix="/", intents=intents)
 
 # Setup journal club commands and tasks
 journal_club_setup(bot)
+ln_refill_setup(bot)
 
 if __name__ == "__main__":
     bot.run(DISCORD_TOKEN)
